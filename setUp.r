9b45c44605f2dff81cb9600ba33452d1f8ae43df
Joel Gsponer <jgsponer@gmail.com>
2015-05-05 12:14:05 +0200

    Update setUp.r

--- setUp.r
+++ setUp.r
@@ -61,6 +61,22 @@ util_sourceFolder <- function(path, lazy=F){
 }
 util_sourceFolder("source/")
 
+#load waRRior
+source_https <- function(url, ...) {
+  # load package
+  require(RCurl)
+ 
+  # parse and evaluate each .R script
+  sapply(c(url, ...), function(u) {
+    eval(parse(text = getURL(u, followlocation = TRUE, cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl"))), envir = .GlobalEnv)
+  })
+}
+ 
+ 
+source_https(
+   "https://raw.githubusercontent.com/joelgsponer/waRRior/master/bootstrap.R"
+)
+
 #Load configuration
 source("config.R")
 
