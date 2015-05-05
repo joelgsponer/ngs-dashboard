0015a77d373f3d62a6c3ce9cecfba71deecc0b8c
Joel Gsponer <jgsponer@gmail.com>
2015-05-05 11:04:36 +0200

    Delete privCuts.r

--- data/coveragefile/privCuts.r
+++
@@ -1,9 +0,0 @@
-                                                   
-privCuts <- function(strataValue){
-    cutoffs <- c()
-    strataValue <- sort(strataValue)
-    for(i in seq(1,length(strataValue))){
-	   cutoffs <- c(cutoffs, (strataValue[i]+strataValue[i+1])/2)
-    }
-	return(cutoffs)
-}
