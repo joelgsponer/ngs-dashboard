971fff100b577b760c786b4fc8951cd884424a5f
Joel Gsponer <jgsponer@gmail.com>
2015-05-05 12:28:56 +0200

    Update config.R

--- config.R
+++ config.R
@@ -1,2 +1,4 @@
-config_VCFSTORAGE = "/data/vcf/"
-config_COVERAGESTORAGE = "/data/coveragefile/"
+config_VCFSTORAGE = "data/vcf/"
+config_COVERAGESTORAGE = "data/coveragefile/"
+config_DBBACKUPSTORAGE = "data/backupdb/"
+config_MAXBACKUPFILES = 10
