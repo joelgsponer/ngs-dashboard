6c60caef454a49a44773f831cb119f266dc32140
Joel Gsponer <jgsponer@gmail.com>
2015-05-20 19:06:27 +0200

    Update functions.r

--- source/functions.r
+++ source/functions.r
@@ -1,79 +1,227 @@
-fncCopyFiles <- function(files, where){
-  ifelse(!(is.null(dim(files))), n <- dim(files)[1], n <- 1)
-  for(i in seq(1, n)){
-    if(!(file.exists(paste(where,files, sep = "/")))){
-      file.copy(files[i,4], paste(where,files[i,1], sep = "/"))
-    }
+setClass("individual",
+  ,slots = c(
+     name       = "character"
+    ,chr        = "list"
+    ,last.score = "numeric"
+  )
+) 
+
+setMethod("show", "individual", function(object){
+  cat("--- Individual ---\n")
+  cat("Name:\t",object@name,"\n")
+  cat("---\n")
+  for(i in seq(1,length(object@chr))){
+    cat(names(object@chr)[i],":\t",object@chr[[i]], "\n")
   }
+  cat("---\n")
+  cat("Last score:\t",object@last.score,"\n")
+  cat("---\n")
+})
+
+create.individual <- function(
+   name = character(0)
+  ,chr
+  ,last.score = numeric(0) 
+  ,create.random.name = F
+){
+  newIndividual <- new("individual")
+  if(create.random.name){
+    require(random)
+    name <-c(randomStrings(n=1, len=10, digits=TRUE, upperalpha=TRUE,loweralpha=TRUE, unique=TRUE, check=TRUE))
+  }
+  newIndividual@name <- name
+  newIndividual@chr <- chr
+  newIndividual@last.score <- last.score  
+  return(newIndividual)
 }
 
-
-
-
-writeLog <- function(message){
-  message <- paste("[",Sys.time(),"]",message,"\n", log.debug, sep = "")
-  assign("log.debug",message, envir = .GlobalEnv)
-}
-
-helpPopup <- function(title, content,
-                      placement=c('right', 'top', 'left', 'bottom'),
-                      trigger=c('click', 'hover', 'focus', 'manual')) {
-  tagList(
-    singleton(
-      tags$head(
-        tags$script("$(function() { $(\"[data-toggle='popover']\").popover(); })")
-      )
-    ),
-    tags$a(
-      href = "#", class = "btn btn-mini", `data-toggle` = "popover",
-      title = title, `data-content` = content, `data-animation` = TRUE,
-      `data-placement` = match.arg(placement, several.ok=TRUE)[1],
-      `data-trigger` = match.arg(trigger, several.ok=TRUE)[1],
-      
-      tags$i(class="icon-question-sign")
+mutation   <- function(
+   chr
+  ,mutation.frequency = 0.01
+  ,genes.class = list("integer")
+  ,genes.range = list(c(0,100)) 
+  ,is.silent = F #it T supresses raising of errors, istead return a list with error information.
+  ,function.id = 'waRRior.machinelearning.genetic_algorithm.mutation' #Use this to identfy the function in error (or success messages if applicable) messages.
+  ,simpleReturn = T
+  ,verbose = T #Turn messages on and off
+  ,debug = F #Turn debug messages on and off
+  ,...
+){
+  tryCatch({
+    if(!(simpleReturn))t <- Sys.time() 
+    #Actual Function code
+    nr_genes <- length(chr)
+    waRRior.snippets.verbose(paste("number of genes:", nr_genes), verbose_ = verbose)
+    if(length(genes.class) != length(genes.range)) stop("nucleotides.class and nucleotides.range must have the same length (either 1 or equal to the number of genes)")
+    if(length(genes.class) == 1 & length(genes.range) == 1){
+       waRRior.snippets.verbose("expanding genes.class and genes genes.range.", verbose_ = verbose)
+       genes.class_ <- genes.class
+       for(i in seq(2, nr_genes))genes.class <- c(genes.class, genes.class_)
+       if(debug)print(genes.class)
+       genes.range_ <- genes.range
+       for(i in seq(2, nr_genes))genes.range <- c(genes.range, genes.range_)
+       if(debug)print(genes.range)
+    }
+    if((length(genes.class) != 1 | length(genes.range) != 1) & (length(genes.class) != nr_genes | length(genes.range) != nr_genes)) stop("genes.class and genes.range have to be either 1 or eual to to the number of genes")
+    is.mutated <- rbinom(nr_genes, 1, mutation.frequency)
+    if(debug)print(is.mutated)
+    new_chr <- chr
+    for(i in seq(1,nr_genes)){
+      if(is.mutated[i]){
+        new_chr[i] <- sample(
+         switch(genes.class[[i]][1],
+             integer = seq(genes.range[[i]][1], genes.range[[i]][2], 1)
+            ,boolean = c(F,T)
+            ,float   = runif(1, genes.range[[i]][1], genes.range[[i]][2])
+         )
+         ,size = 1
+         ,replace = T
+        )
+        if(debug)print(new_chr[i])
+      }
+    }
+    if(debug)print(new_chr)
+    #define response
+    if(simpleReturn)return(new_chr)
+    response <- list(
+       result <- as.list(new_chr)
+      ,message <- paste(function.id, 'success')
+      ,error = 0
+      ,e = NULL
+      ,time.elapsed = as.numeric(difftime(Sys.time(), t), units = "secs")
     )
+    #Success message
+    waRRior.snippets.verbose(paste(function.id,'success'), verbose_ = verbose)
+    #Return respond
+    return(response)
+   #Error handling
+   }, error = function(e, silent = is.silent){
+     #If silent supress error and return list instead
+      if(silent){
+       return(list(
+          message = sprintf("Error during function: %s", function.id)
+         ,error = 1
+         ,e = e
+       ))
+      }else{
+        stop(e)
+      }
+     }
   )
 }
 
-disableActionButton <- function(id,session) {
-  session$sendCustomMessage(type="jsCode",
-                             list(code= paste("$('#",id,"').prop('disabled',true)"
-                                    ,sep="")))
-}
-enableActionButton <- function(id,session) {
-  session$sendCustomMessage(type="jsCode",
-                             list(code= paste("$('#",id,"').prop('disabled',false)"
-                                    ,sep="")))
-} 
+crossover <- function(
+   #The chromosomes can be passed as a list.
+   ...
+  ,is.silent = F #it T supresses raising of errors, istead return a list with error information.
+  ,function.id = 'waRRior.machinelearning.genetic_algorithm.crossover' #Use this to identfy the function in error (or success messages if applicable) messages.
+  ,verbose = T #Turn messages on and off
+  ,debug = T #Turn debug messages on and off
+){
+  tryCatch({
+    t <- Sys.time() 
+    #Actual Function code
+    chrs <- list(...)
 
-updateModifyFields <- function(session_, fields, tables){
-  tryCatch({
-    r <- function(table){
-      strSQL <- sprintf('SELECT ngsfacilityid FROM %s', table)
-      x <-  dbGetQuery(db, strSQL)
-      return(rev(c(x$ngsfacilityid)))
+    
+    nr_of_chrs <- length(chrs)
+    waRRior.snippets.verbose(paste('nr of chromosomes:', nr_of_chrs), verbose_ = verbose)
+
+    nr_of_genes <- length(chrs[[1]])
+    
+    mat <- matrix(unlist(chrs),nr_of_genes, nr_of_chrs)
+    colnames(mat) <- names(chrs)
+    rownames(mat) <- names(chrs[[1]])
+    
+    if(verbose){
+    	waRRior.snippets.verbose('avaiable chromosomes:', verbose_ = verbose)
+    	print(mat)  	
     }
-  }, error = function(e){
-    return(list(
-      message = 'Error while fetching ngsfacilitzid form table'
-      ,error = 1
-      ,e = e
-      ))
-    })
-  tryCatch({
-    for(field in fields){
-      updateSelectInput(
-         session_
-        ,inputId   = field
-        ,choices   = c('NEW RECORD',r(table = tables[which(fields == field)]))
-        ,selected  = 'NEW RECORD'
-      )
-    }
-  }, error = function(e){
-    return(list(
-       message = 'Error while updating fields with ngsfacilityid'
-      ,error = 1
-      ,e = e
-    ))
-  })
+    
+    if(nr_of_genes != mean(unlist(lapply(chrs, FUN = length))))stop("chromosomes must have equal length (number of genes)")
+    waRRior.snippets.verbose(paste('nr of genes:', nr_of_genes), verbose_ = verbose)
+    
+    
+    new_chr <- apply(mat, 1, FUN = sample, size = 1) 
+    if(debug) print(new_chr)
+        
+    #define response
+    response <- list(
+       result <- as.list(new_chr)
+      ,message <- paste(function.id, 'success')
+      ,error = 0
+      ,e = NULL
+      ,time.elapsed = as.numeric(difftime(Sys.time(), t), units = "secs")
+    )
+    
+    #Success message
+    waRRior.snippets.verbose(paste(function.id,'success'), verbose_ = verbose)
+    
+    #Return respond
+    return(response)
+  
+   #Error handling
+   }, error = function(e, silent = is.silent){
+     #If silent supress error and return list instead
+      if(silent){
+       return(list(
+          message = sprintf("Error during function: %s", function.id)
+         ,error = 1
+         ,e = e
+       ))
+      }else{
+        stop(e)
+      }
+     }
+  )
 }
+
+evaluate <- function(object, test.data){
+  
+  #Function code
+  x <- seq(1, 100)
+  prediction <- x ^ object@chr$a + object@chr$b
+  
+  score <- sum((test.data - prediction)^2)
+  #Retrun Score
+  return(score)
+}
+
+x <- seq(1,100)
+test.data <- x ^ 3 + 4
+
+#Parameters
+population.size <- 10
+chr.init <- list(
+   a = 1
+  ,b = 2
+)
+
+#Initalization
+population <- list()
+
+for(i in seq(1, population.size)){
+  population <- c(population, create.individual(
+     chr = mutation(chr.init
+       ,mutation.frequency = 1
+       ,verbose = F
+       ) 
+    ,create.random.name=T
+    )
+  )  
+}
+
+for(i in seq(1, population.size)){
+  s <- evaluate(population[[i]], test.data)
+  population[[i]]@last.score <- s
+}
+
+#Run
+
+
+
+
+
+
+
+
